f73bcb222deae67ec11f0e6e5b44082ffbc46609
hoannguyen <hoannguyen1609@gmail.com>
2022-04-29 17:11:20 +0700

    Update sparkMLiris.py

--- sparkMLiris.py
+++ sparkMLiris.py
@@ -6,4 +6,25 @@ spark = SparkSession.builder.master("local") \
                             .config("spark.executor.memory", "lgb") \
                             .getOrCreate()
 
-#rdd = sc.textFile
+#rdd = sc.textFile('/Users/Storage/Vicohub/Data/Iris.csv’)
+df = spark.read.csv('/Users/Storage/Vicohub/Data/Iris.csv', header=True)
+input_data = df.rdd.map(lambda x: (DenseVector(x[:4]), x[4]))
+df1 = spark.createDataFrame(input_data, ["X", "Y"])
+
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml.classification import RandomForestClassifier
+indexer = StringIndexer(inputCol="Y", outputCol="Yn")
+indexed = indexer.fit(df1).transform(df1)
+(trainingData, testData) = indexed.randomSplit([0.7, 0.3])
+rf = RandomForestClassifier(labelCol="Yn",
+featuresCol="X",
+numTrees=10)
+model = rf.fit(trainingData)
+predictions = model.transform(testData)
+
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+predictions.select("Yn", "prediction")
+evaluator = MulticlassClassificationEvaluator(
+labelCol="Yn", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("Test Accuracy = %g" % (accuracy))
